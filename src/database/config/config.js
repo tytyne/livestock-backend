978907b862fbd911685502f4d50b07b0947c9931
dusabeyezu florentine <dusaflora2@gmail.com>
2022-01-12 11:43:29 +0200

    change config file

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,16 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
-    dialect: 'postgres',
+    dialect: "postgres",
+    logging: false,
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    dialect: 'postgres',
+    dialect: "postgres",
+    logging: false,
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    dialect: "postgres",
+    dialectOptions:{
+      ssl:{
+        require:true,
+        rejectUnauthorized:false
+      }
+    }
   },
-}
+};
