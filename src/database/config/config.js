842bde4c3426febc3cc24318660cd36633741287
tytyne <dusaflora2@gmail.com>
2022-07-12 12:18:27 +0200

    change config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,11 +14,6 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
-    dialectOptions:{
-      ssl:{
-        require:true,
-        rejectUnauthorized:false
-      }
-    }
+    
   },
 };
