73a091b82a76211b4b8177b21e7b8ac8cea6421b
akashi7 <christiannseko@gmail.com>
2022-10-26 12:27:12 +0200

    nothing serious

--- src/validation/reset.validator.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi'
-
-export  const emailVerify=Joi.object({                                                                                                                                
-    email:Joi.string().email().required()
-})
-export const resetPassword=Joi.object({                                                                                                                                
-    password:Joi.string().min(8).required(),
-    confirmPassword:Joi.string().required()
-})
