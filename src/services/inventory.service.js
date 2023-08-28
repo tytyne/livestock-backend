9a6acc810d20ab254ee3d682993a2c15be4267b9
tytyne <dusaflora2@gmail.com>
2023-08-28 22:35:59 +0200

    Adding inventory service

---
+++ src/services/inventory.service.js
@@ -0,0 +1,38 @@
+import models from "../database/models/index"
+import "regenerator-runtime/runtime";
+const{Stocking} = models;
+
+
+/**
+ * @description This model deals with Inventory model
+ */
+
+class InventoryService{
+    static async storeInventory(value){
+        let inventory = await Stocking.create(value)
+        return inventory
+    }
+    static async getInventoryById(id){
+        let inventory = await Stocking.findOne({where:{id:id}})
+        return inventory
+
+    }
+    static async getAllInventories(farmId){
+        let inventory = await Stocking.findAll({where:{farm_id:farmId}})
+        return inventory
+
+    }
+    static async updateInventoryById(id,value){
+        let inventory = await Stocking.update(value,{where:{id:id}})
+        return inventory
+
+    }
+    
+    static async deleteInventoryById(id){
+        let inventory = await Stocking.destroy({where:{id:id}})
+        return inventory
+    }
+
+
+}
+export default InventoryService
